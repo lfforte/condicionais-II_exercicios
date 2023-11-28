a7a259f9a55dfe8dc6013eb95585946c843e85bb
LuizFernandoForte <lfforte@gmail.com>
2023-11-28 14:05:31 -0300

    Solução exercício 03

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,16 +1,23 @@
 //códigos a serem reescritos
-
+const funcaoTeste = (nome, idade) =>{
 // a)
-if(nome === "José"){
+/* if(nome === "José"){
 	console.log("Oi, Zé!")
 } else {
 	console.log("Olá, " + nome)
-}
+} */
+nome === "José" ? console.log("Oi, Zé!") : console.log("Olá, " + nome);
 
 
 // b)
-if(idade >= 18){
+/* if(idade >= 18){
 	console.log("Pode tirar carteira de motorista!")
 } else {
 	console.log("Ainda não pode tirar carteira de motorista!")
+} */
+
+idade >= 18 ? console.log("Pode tirar carteira de motorista!") : console.log("Ainda não pode tirar carteira de motorista!");
+
 }
+
+funcaoTeste("José", 14);
